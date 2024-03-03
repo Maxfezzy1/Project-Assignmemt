dc30f0e6e46dbba6b452915b91621af3f85343f3
ifenna ezeh <maxfezzy@gmail.com>
2024-03-03 15:29:01 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Please Enter your Fahrenheit Temperature you wish to convert to Celsius:");
+        var UserFahrenheit = Convert.ToDouble(Console.ReadLine());
+
+        var Celsius = (UserFahrenheit - 32) * 5 / 9;
+
+        Console.WriteLine("The Fahrenheit Temperature you wish to convert to Celsius is {0}: ", Celsius);
+
+
+
+
+        Console.Write("Please Enter your Celsius Temperature you wish to convert to Fahrenheit: ");
+        var celsius = Convert.ToDouble(Console.ReadLine());
+
+        var fahrenheit = ((celsius * 9) / 5) + 32;
+
+        Console.WriteLine(" The Celsius Temperature you wish to convert to Fahrenheit is {0}: " , fahrenheit);
+        
+
+
+
+    }
+
+
+
+
+}
+   
+
+
+
+
